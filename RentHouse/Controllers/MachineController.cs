a233dd4ed13db76526e007a700eecb98ed94fc22
bartek <barte@barte.pl>
2023-10-29 12:14:45 +0100

    small controller upgrades

--- RentHouse/Controllers/MachineController.cs
+++ RentHouse/Controllers/MachineController.cs
@@ -1,25 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RentHouse.Models;
+using RentHouse.Data;
+using System.Linq;
 
 namespace RentHouse.Controllers;
 
 public class MachineController : Controller
 {
     private readonly ILogger<MachineController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public MachineController(ILogger<MachineController> logger)
+    public MachineController(ILogger<MachineController> logger, ApplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Details(int? Id)
     {
-        if(!Id.hasValue){
-        
+        if(!Id.HasValue){
+            return Json(_db.Machines.ToList());
         }else{
-            
-            return Json();
+            //string query = "SELECT * FROM Machines WHERE ID = @p0"
+            MachineModel machine = -_db.Machines.FirstOrDeafult(machine => machine.Id == Id);
+            return Json(machine);
         }
     }
 
